8420c41a4795db6cb08597a6df5b20748075a2c2
Daniel Gunna <danielgunna1408@gmail.com>
2018-05-22 20:05:35 -0300

    code clean up

--- src/main/java/controller/JFlapLambdaConverter.java
+++ src/main/java/controller/JFlapLambdaConverter.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 public class JFlapLambdaConverter {
     static boolean stop = false;
+
     public static void main(String[] args) {
         JFileChooser chooser = new JFileChooser();
         chooser.addChoosableFileFilter(new FileNameExtensionFilter("Arquivos JFLAP4", ".jff"));
@@ -24,33 +25,36 @@ public class JFlapLambdaConverter {
                 new DfaAutomatonSimulator().makeSimulation(automaton, input, new DfaAutomatonSimulator.SimulationListener() {
                     @Override
                     public void onAccept(String simulation) {
-                        if (JOptionPane.showConfirmDialog(null, simulation + "\nDeseja salvar o DFA gerado?") == JOptionPane.OK_OPTION) {
-                            JFileChooser chooser = new JFileChooser();
-                            chooser.setCurrentDirectory(new java.io.File("."));
-                            chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-                            chooser.setAcceptAllFileFilterUsed(false);
-                            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-                                AutomatonWriter writer = new AutomatonWriter();
-                                try {
-                                    writer.saveJflapFile(writer.getJflapFileContentFromAutomaton(automaton),chooser.getCurrentDirectory().getAbsolutePath());
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }
+                        saveDfa(automaton, simulation + "\nA entrada pertence a linguagem reconhecida pelo automato.");
                     }
 
                     @Override
                     public void onError(String msg, String simulation) {
-                        if (!(JOptionPane.showConfirmDialog(null, simulation + "\nDeseja continuar?") == JOptionPane.OK_OPTION)){
+                        if (!(JOptionPane.showConfirmDialog(null, simulation + "\n" + msg + "\nDeseja inserir uma nova entrada?") == JOptionPane.OK_OPTION)) {
                             stop = true;
+                            saveDfa(automaton, simulation);
                         }
                     }
                 });
             }
-
-
         }
 
+    }
+
+    private static void saveDfa(Automaton automaton, String simulation) {
+        if (JOptionPane.showConfirmDialog(null, simulation + "\nDeseja salvar o DFA gerado?") == JOptionPane.OK_OPTION) {
+            JFileChooser chooser = new JFileChooser();
+            chooser.setCurrentDirectory(new java.io.File("."));
+            chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            chooser.setAcceptAllFileFilterUsed(false);
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                AutomatonWriter writer = new AutomatonWriter();
+                try {
+                    writer.saveJflapFile(writer.getJflapFileContentFromAutomaton(automaton), chooser.getCurrentDirectory().getAbsolutePath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
